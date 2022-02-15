ca6b90660231b45833bf82592933899f79664658
Alexandru Bangau <bangau.alexandru@gmail.com>
2022-02-15 08:55:17 +0100

    WIP - Show time in relative format, styling updates

--- src/Components/Quotes/Quote.tsx
+++ src/Components/Quotes/Quote.tsx
@@ -1,5 +1,8 @@
-import moment from "moment";
 import React from "react";
+import moment from "moment";
+
+import headshotImg from '../../assets/images/png/headshot.png';
+import replyIcon from '../../assets/images/svg/reply.svg';
 
 import './Quote.scss';
 
@@ -22,15 +25,25 @@ export const Quote = ({ quote } : IProps) => {
   return (
     <div className="quote-wrapper">
       <div className="quote-wrapper__votes">{quote.likes}</div>
-      <div className="quotes-wrapper__details">
-        <div className="quotes-wrapper__details-header">
-          <div className="quotes-wrapper__details-header-user">
-            <div className="quotes-wrapper__details-header-user-name">{quote.authorName}</div>
+      <div className="quote-wrapper__details">
+        <div className="quote-wrapper__details-header">
+          <div className="quote-wrapper__details-header-user">
+            <div className="quote-wrapper__details-header-user quote-wrapper__details-user-name">
+              <img
+                src={headshotImg}
+                className="quote-wrapper__details-header-headshot"
+                alt="user profile headshot"
+              />
+              {quote.authorName}
             </div>
-          <span className="quotes-wrapper__details-header-user-timestamp">{moment(quote.created.toDate()).calendar()}</span>
-          <div className="quotes-wrapper__details-header-reply">Reply</div>
+            <span className="quotes-wrapper__details-header-user-timestamp">{moment(quote.created.toDate(), "YYYYMMDD").fromNow()}</span>
+          </div>
+          <div className="quote-wrapper__details-header-reply">
+            <img src={replyIcon} alt="reply back" className="quote-wrapper__details-header-reply-img" />
+            Reply
+          </div>
         </div>
-        <div className="quotes-wrapper__body">{quote.body}</div>
+        <div className="quote-wrapper__body">{quote.body}</div>
       </div>
     </div>
   );
