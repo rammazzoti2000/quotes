f78c1e10d4b7a4126e0704c6db0eaf7729626670
Alexandru Bangau <bangau.alexandru@gmail.com>
2022-02-23 09:31:50 +0100

    Display Quotes - Finish styling

--- src/Components/Quotes/Quote.tsx
+++ src/Components/Quotes/Quote.tsx
@@ -24,8 +24,12 @@ interface IProps {
 export const Quote = ({ quote } : IProps) => {
   return (
     <div className="quote-wrapper">
-      <div className="quote-wrapper__votes">{quote.likes}</div>
-      <div className="quote-wrapper__details">
+      <div className="quote-wrapper__votes">
+        <button className="quote-wrapper__votes-button quote-wrapper__votes-button-plus" type="button">+</button>
+          {quote.likes}
+        <button className="quote-wrapper__votes-button quote-wrapper__votes-button-minus" type="button">-</button>
+      </div>
+      <article className="quote-wrapper__details">
         <div className="quote-wrapper__details-header">
           <div className="quote-wrapper__details-header-user">
             <div className="quote-wrapper__details-header-user quote-wrapper__details-user-name">
@@ -38,13 +42,16 @@ export const Quote = ({ quote } : IProps) => {
             </div>
             <span className="quotes-wrapper__details-header-user-timestamp">{moment(quote.created.toDate(), "YYYYMMDD").fromNow()}</span>
           </div>
-          <p className="quote-wrapper__details-header-reply">
+          <div className="quote-wrapper__details-header-reply">
             <img src={replyIcon} alt="reply back" className="quote-wrapper__details-header-reply-img" />
             Reply
-          </p>
+          </div>
         </div>
-        <div className="quote-wrapper__body">{quote.body}</div>
-      </div>
+        <p className="quote-wrapper__body">
+          {quote.body}
+        </p>
+      </article>
+      <span className="quote-wrapper__hashtags">{quote.hashtags.map(item => `#${item} `)}</span>
     </div>
   );
 };
