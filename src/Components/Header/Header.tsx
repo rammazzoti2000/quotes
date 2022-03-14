e5a37c37c1895a6d4e4bb66ab5e6d1b4c3988978
Alexandru Bangau <bangau.alexandru@gmail.com>
2022-03-14 09:18:05 +0100

    WIP - Create quote modal fields with validation

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -4,6 +4,75 @@ import './Header.scss';
 
 export const Header = () => {
   const [showModal, setShowModal] = useState(false);
+  const [author, setAuthor] = useState('');
+  const [hashtags, setHashtags] = useState<string[]>([]);
+  const [quoteBody, setQuoteBody] = useState('');
+  const [displayError, setDisplayError] = useState(false);
+  const [error, setError] = useState({
+    author: '',
+    hashtags: [],
+    quoteBody: '',
+  });
+
+  const cleanErrors = () => {
+    setError({
+      author: '',
+      hashtags: [],
+      quoteBody: '',
+    });
+    setDisplayError(false);
+  };
+
+  const cleanData = () => {
+    setAuthor('');
+    setHashtags([]);
+    setQuoteBody('');
+  };
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    let shouldDisplay = false;
+    const tempError = {
+      author: '',
+      hashtags: [],
+      quoteBody: '',
+    };
+
+    if (author.trim() === '') {
+      tempError.author = 'Please fill in Author.';
+      shouldDisplay = true;
+    }
+
+    // if (!hashtags.length) {
+    //   tempError.author = 'Please fill in minimum one hashtag.';
+    //   shouldDisplay = true;
+    // }
+
+    if (quoteBody.trim() === '') {
+      tempError.author = 'Please fill in Quote.';
+      shouldDisplay = true;
+    }
+
+    if (shouldDisplay) {
+      setError(tempError);
+      setDisplayError(true);
+      return;
+    }
+  }
+
+  const handleChangeAuthor = (event: any) => {
+    setAuthor(event.target.value);
+    cleanErrors();
+  };
+
+  // const handleChangeHashtags = (event: any) => {
+    
+  // }
+
+  const handleChangeQuote = (event: any) => {
+    setQuoteBody(event.target.value);
+    cleanErrors();
+  }
 
   const handleShowModal = () => {
     setShowModal(!showModal)
@@ -24,7 +93,27 @@ export const Header = () => {
       </button>
       <CustomModal show={showModal} handleClose={handleCloseModal}>
         <div>
-          Modal is working
+        <div>
+          <form onSubmit={handleSubmit}>
+            <div className="quotes-modal__inputs">
+              <label>
+                Author
+                  <input type="text" value={author} onChange={handleChangeAuthor} />
+                  <span className="quotes-modal__field-error">
+                    {displayError && error.author}
+                  </span>
+              </label>
+              <label>
+                Quote
+                <input type="text" value={quoteBody} onChange={handleChangeQuote} />
+                <span className="quotes-modal__field-error">
+                  {displayError && error.quoteBody}
+                </span>
+              </label>
+            </div>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
         </div>
       </CustomModal>
     </header>
